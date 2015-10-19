df5a9024a606c8b06e1bec56775d9244ed2e4c96
Ana Daniel <anadanield@gmail.com>
2015-10-19 12:12:33 +0200

    Print people that meet a weight condition

--- HealthProfileReaderXML/src/HealthProfileReader.java
+++ HealthProfileReaderXML/src/HealthProfileReader.java
@@ -67,6 +67,29 @@ public class HealthProfileReader {
   }
 
   /**
+  * Print a list of all people in the people.xml file that fullfill the given weight condition
+  *
+  * @param condition  The weight condition for the people to print (i.e. '>80', '=75Kg')
+  */
+  public void printPeople(String condition) throws XPathExpressionException {
+
+    char operator = condition.charAt(0);
+    String weight = condition.substring(1);
+
+    XPathExpression expr = xpath.compile("//person[healthprofile/weight" + operator + "'" + weight + "']");
+    NodeList person_nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+    Integer number_of_people = person_nodes.getLength();
+
+    for (int i = 0; i < number_of_people; i++) {
+
+      Node person = person_nodes.item(i);
+      printPerson(person);
+      printHealthProfile(person);
+    }
+  }
+
+  /**
   * Print a list of all people in the people.xml file
   */
   public void printPeople() throws XPathExpressionException {
@@ -80,6 +103,7 @@ public class HealthProfileReader {
 
       Node person = person_nodes.item(i);
       printPerson(person);
+      printHealthProfile(person);
     }
   }
 
@@ -117,8 +141,8 @@ public class HealthProfileReader {
     Element person_element = (Element) person;
     Element health_profile = (Element) person_element.getElementsByTagName("healthprofile").item(0);
 
-    System.out.println( "Height: " + health_profile.getElementsByTagName("weight").item(0).getTextContent() );
-    System.out.println( "Weight: " + health_profile.getElementsByTagName("height").item(0).getTextContent() );
+    System.out.println( "Weight: " + health_profile.getElementsByTagName("weight").item(0).getTextContent() );
+    System.out.println( "Height: " + health_profile.getElementsByTagName("height").item(0).getTextContent() );
     System.out.println( "BMI: " + health_profile.getElementsByTagName("bmi").item(0).getTextContent() );
   }
 
@@ -135,55 +159,61 @@ public class HealthProfileReader {
     }
   }
 
-    /**
-     * The health profile reader gets information from the command line about
-     * weight and height and calculates the BMI of the person based on this 
-     * parameters
-     * 
-     * @param args
-     */
-    public static void main(String[] args) throws ParserConfigurationException, SAXException,
-    IOException, XPathExpressionException {
-      int argCount = args.length;
+  /**
+   * The health profile reader gets information from the command line about
+   * weight and height and calculates the BMI of the person based on this 
+   * parameters
+   * 
+   * @param args
+   */
+  public static void main(String[] args) throws ParserConfigurationException, SAXException,
+  IOException, XPathExpressionException {
+    int argCount = args.length;
 
-      if (argCount >= 1) {
+    if (argCount >= 1) {
 
-        HealthProfileReader profileReader = new HealthProfileReader();
-        profileReader.loadXML();
+      HealthProfileReader profileReader = new HealthProfileReader();
+      profileReader.loadXML();
 
-        // Process getWeight and getHeight functions
-        if ( args[0].equals("getWeight") || args[0].equals("getHeight") ) {
-          if (args[1] != null && args[2] != null ) {
-            Node result = null;
+      // Process getWeight and getHeight functions
+      if ( args[0].equals("getWeight") || args[0].equals("getHeight") ) {
+        if ( argCount >= 3 ) {
+          Node result = null;
 
-            if ( args[0].equals("getWeight") ) {
-              result = profileReader.getWeight(args[1], args[2]);
-            }
-            else {
-              result = profileReader.getHeight(args[1], args[2]);
-            }
+          if ( args[0].equals("getWeight") ) {
+            result = profileReader.getWeight(args[1], args[2]);
+          }
+          else {
+            result = profileReader.getHeight(args[1], args[2]);
+          }
 
-            if (result != null){
-              System.out.println( result.getTextContent()  );
-            }
-            else {
-              System.out.println("Person not found!");
-            }
+          if (result != null){
+            System.out.println( result.getTextContent()  );
+          }
+          else {
+            System.out.println("Person not found!");
+          }
 
-          } else {
-            System.out.println("I need a first and last name to retreive a person's data");
+        } else {
+          System.out.println("I need a first and last name to retreive a person's data");
+        }
+      }
+      else {
+        if ( args[0].equals("printPeople") ) {
+          if ( argCount >= 2 ){
+            System.out.println(args[1]);
+            profileReader.printPeople(args[1]);
+          }
+          else{
+            profileReader.printPeople();
           }
         }
         else {
-          if ( args[0].equals("printPeople") ) {
-            profileReader.printPeople();
-          }
-          else {
-            if ( args[0].equals("printHealthProfileFromId") ){
-              profileReader.printHealthProfileFromId( args[1] );
-            }
+          if ( args[0].equals("printHealthProfileFromId") ){
+            profileReader.printHealthProfileFromId( args[1] );
           }
         }
       }
     }
   }
+}
